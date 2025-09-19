989e2527484b5280e1ae97be6d3a23eb29d86e5f
Sandman90 <C0llaud0!>
2025-09-19 02:41:37 +0200

    Brush.

--- src/functions/brush.ts
+++ src/functions/brush.ts
@@ -15,18 +15,33 @@ function brush(brushType: BrushType, squaresArray: HTMLElement[], e: EventTarget
 
 function brushCross(squaresArray: HTMLElement[], e: EventTarget | null, squares: NodeListOf<HTMLElement>, cols: number) {
     const currentIndex = squaresArray.indexOf(e as HTMLElement);
+    // Up/Down.
     const aboveIndex = currentIndex - cols;
-    const belowIndex = currentIndex + cols;
     if (aboveIndex >= 0) squares[aboveIndex].classList.add('active');
+    const belowIndex = currentIndex + cols;
     if (belowIndex < squares.length) squares[belowIndex].classList.add('active');
+    // Left/Right.
+    const beforeIndex = currentIndex - 1;
+    if (beforeIndex >= 0 && beforeIndex % cols !== (cols-1)) squares[beforeIndex].classList.add('active');
+    const afterIndex = currentIndex + 1;
+    if (afterIndex < squares.length && afterIndex % cols !== 0) squares[afterIndex].classList.add('active');
+    // console.log('beforeIndex: ', beforeIndex, 'afterIndex:', afterIndex, 'Mod: ', beforeIndex % cols, (cols-1));
 }
 
 function brushBox(squaresArray: HTMLElement[], e: EventTarget | null, squares: NodeListOf<HTMLElement>, cols: number) {
     const currentIndex = squaresArray.indexOf(e as HTMLElement);
+    // Up/Down.
     const aboveIndex = currentIndex - cols;
+    if (aboveIndex - 1 >= 0) {
+        squares[aboveIndex - 1].classList.add('active');
+        squares[aboveIndex + 1].classList.add('active');
+    }
     const belowIndex = currentIndex + cols;
-    if (aboveIndex >= 0) squares[aboveIndex].classList.add('active');
-    if (belowIndex < squares.length) squares[belowIndex].classList.add('active');
+    if (belowIndex + 1 < squares.length) {
+        squares[belowIndex - 1].classList.add('active');
+        squares[belowIndex + 1].classList.add('active');
+    }
+    brushCross(squaresArray, e, squares, cols);
 }
 
 export { BrushType, brush };
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import createGrid from "./functions/grid";
 document.addEventListener('DOMContentLoaded', () => {
     // Active every square selected.
     const createGridBtn: HTMLButtonElement = document.getElementById('create-grid-btn') as HTMLButtonElement;
-    const rowsInput: HTMLInputElement = document.getElementById('rows') as HTMLInputElement;
-    const rows: number = parseInt(rowsInput.value);
-    const colsInput: HTMLInputElement = document.getElementById('cols') as HTMLInputElement;
-    const cols: number = parseInt(colsInput.value);
     createGridBtn.addEventListener('click', () => {
-        if (rows > 0 && cols > 0) createGrid(rows, cols, 'cross'); // Create grid.
+        const rows: number = parseInt((document.getElementById('rows') as HTMLInputElement).value);
+        const cols: number = parseInt((document.getElementById('cols') as HTMLInputElement).value);
+        if (rows > 0 && cols > 0) createGrid(rows, cols, 'box'); // Create grid.
         else alert('Inserisci valori validi per righe e colonne.');
     });
 
@@ -17,5 +15,5 @@ document.addEventListener('DOMContentLoaded', () => {
     randomizeBtn.addEventListener('click', randomizeSquare); // Chiama la funzione di randomizzazione al click del pulsante
 
     // Crea una griglia di dimensioni predefinite all'avvio della pagina
-    createGrid(15, 15, 'cross');
+    createGrid(15, 15, 'box');
 });
