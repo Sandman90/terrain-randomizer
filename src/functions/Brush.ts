cd3ae433f54426083c588532f7ae18bae0bc7147
Sandman90 <C0llaud0!>
2025-09-23 23:55:48 +0200

    Brush + others.

---
+++ src/functions/Brush.ts
@@ -0,0 +1,48 @@
+type BrushType = 'point' | 'cross' | 'box';
+
+function brush(brushType: BrushType, squaresArray: HTMLElement[], e: EventTarget | null, squares: NodeListOf<HTMLElement>, cols: number) {
+  switch (brushType) {
+    case "cross":
+      brushCross(squaresArray, e, squares, cols);
+      break;
+    case "box":
+      brushBox(squaresArray, e, squares, cols);
+      break;
+    default:
+    case "point":
+      break;
+  }
+}
+
+function brushCross(squaresArray: HTMLElement[], e: EventTarget | null, squares: NodeListOf<HTMLElement>, cols: number) {
+  const currentIndex = squaresArray.indexOf(e as HTMLElement);
+  // Up/Down.
+  const aboveIndex = currentIndex - cols;
+  if (aboveIndex >= 0) squares[aboveIndex].classList.add('active');
+  const belowIndex = currentIndex + cols;
+  if (belowIndex < squares.length) squares[belowIndex].classList.add('active');
+  // Left/Right.
+  const beforeIndex = currentIndex - 1;
+  if (beforeIndex >= 0 && beforeIndex % cols !== (cols-1)) squares[beforeIndex].classList.add('active');
+  const afterIndex = currentIndex + 1;
+  if (afterIndex < squares.length && afterIndex % cols !== 0) squares[afterIndex].classList.add('active');
+  // console.log('beforeIndex: ', beforeIndex, 'afterIndex:', afterIndex, 'Mod: ', beforeIndex % cols, (cols-1));
+}
+
+function brushBox(squaresArray: HTMLElement[], e: EventTarget | null, squares: NodeListOf<HTMLElement>, cols: number) {
+  const currentIndex = squaresArray.indexOf(e as HTMLElement);
+  // Up/Down.
+  const aboveIndex = currentIndex - cols;
+  if (aboveIndex - 1 >= 0) {
+    squares[aboveIndex - 1].classList.add('active');
+    squares[aboveIndex + 1].classList.add('active');
+  }
+  const belowIndex = currentIndex + cols;
+  if (belowIndex + 1 < squares.length) {
+    squares[belowIndex - 1].classList.add('active');
+    squares[belowIndex + 1].classList.add('active');
+  }
+  brushCross(squaresArray, e, squares, cols);
+}
+
+export { BrushType, brush };
--- src/functions/randomizeSquare.ts
+++ src/functions/randomizeSquare.ts
@@ -1,21 +1,21 @@
 
 // Funzione per randomizzare un quadrato con annotazioni di tipo
 export default function randomizeSquare(): void {
-    // Seleziona tutti gli elementi con la classe 'grid-square'
-    const squares: NodeListOf<HTMLElement> = document.querySelectorAll('.grid-square.active');
+  // Seleziona tutti gli elementi con la classe 'grid-square'
+  const squares: NodeListOf<HTMLElement> = document.querySelectorAll('.grid-square.active');
 
-    // Verifica se ci sono quadrati nella griglia
-    if (squares.length === 0) {
-        alert('Per favore, crea prima una griglia!');
-        return;
-    }
+  // Verifica se ci sono quadrati nella griglia
+  if (squares.length === 0) {
+    alert('Per favore, crea prima una griglia!');
+    return;
+  }
 
-    // Rimuove la classe 'selected' da tutti i quadrati per deselezionare quello precedentemente evidenziato
-    squares.forEach(square => square.classList.remove('selected'));
+  // Rimuove la classe 'selected' da tutti i quadrati per deselezionare quello precedentemente evidenziato
+  squares.forEach(square => square.classList.remove('selected'));
 
-    // Calcola un indice casuale all'interno dell'array di quadrati
-    const randomIndex: number = Math.floor(Math.random() * squares.length);
+  // Calcola un indice casuale all'interno dell'array di quadrati
+  const randomIndex: number = Math.floor(Math.random() * squares.length);
 
-    // Aggiunge la classe 'selected' al quadrato scelto casualmente
-    squares[randomIndex].classList.add('selected');
+  // Aggiunge la classe 'selected' al quadrato scelto casualmente
+  squares[randomIndex].classList.add('selected');
 }
