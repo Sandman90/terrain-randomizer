95b662ffb5e6258e42388e18a4735b9aa08faa25
Sandman90 <C0llaud0!>
2025-09-27 03:03:12 +0200

    Styles.

--- src/functions/Save.ts
+++ src/functions/Save.ts
@@ -80,38 +80,6 @@ function loadMapFromLocalStorage(mapName: string): MapData | null {
   }
 }
 
-// function loadMapFromLocalStorage(gridContainer: HTMLElement, mapName: string): void {
-//   // Recupera la stringa serializzata
-//   const serializedMap = localStorage.getItem('savedMap_' + mapNameFn(mapName));
-//
-//   if (serializedMap) {
-//     try {
-//       // Deserializza la stringa in un array di oggetti
-//       const mapData: SquareState[] = JSON.parse(serializedMap);
-//       const squares = Array.from(gridContainer.children) as HTMLElement[];
-//
-//       // Assicurati che le dimensioni della griglia corrispondano ai dati salvati
-//       if (mapData.length === squares.length) {
-//         // Applica lo stato salvato a ogni quadrato della griglia
-//         mapData.forEach((data, index) => {
-//           if (data.s) {
-//             squares[index].classList.add('active');
-//           } else {
-//             squares[index].classList.remove('active');
-//           }
-//         });
-//         console.log('Mappa caricata con successo.');
-//       } else {
-//         console.warn('Le dimensioni della griglia non corrispondono ai dati salvati.');
-//       }
-//     } catch (e) {
-//       console.error('Errore durante la deserializzazione dei dati.', e);
-//     }
-//   } else {
-//     console.log('Nessuna mappa salvata trovata.');
-//   }
-// }
-
 function getSavedMapNames(): void {
   const savedNames = localStorage.getItem('mapNames');
   let savedNamesArray: string[] = [];
